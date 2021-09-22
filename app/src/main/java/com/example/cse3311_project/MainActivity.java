bede5e5620005ddcd37dbb1c36db168032bf127d
michael.stokes <cse3311.team3@gmail.com>
2021-09-21 21:10:38 -0500

    test

---
+++ app/src/main/java/com/example/cse3311_project/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.cse3311_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String email;
+    String password;
+
+    EditText emailInput;
+    EditText passwordInput;
+
+    Button loginButton;
+    Button createAccountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        createAccountButton = (Button) findViewById(R.id.createAccountButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
+            }
+        });
+    }
+}
