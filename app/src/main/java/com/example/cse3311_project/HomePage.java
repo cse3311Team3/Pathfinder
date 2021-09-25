d85672f0e340b61697ab8a33e739fa4eea111962
Yunika Upadhayaya <yupadhayaya08@gmail.com>
2021-09-25 18:48:01 -0500

    adding toolbox in homepage

--- app/src/main/java/com/example/cse3311_project/HomePage.java
+++ app/src/main/java/com/example/cse3311_project/HomePage.java
@@ -1,11 +1,17 @@
 package com.example.cse3311_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class HomePage extends AppCompatActivity implements View.OnClickListener  {
@@ -16,7 +22,11 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         current_schedule = (CardView) findViewById(R.id.c1);
         create_schedule = (CardView) findViewById(R.id.c2);
         edit_route = (CardView) findViewById(R.id.c3);
@@ -28,9 +38,46 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
         edit_route.setOnClickListener(this);
         view_map.setOnClickListener(this);
         list_view.setOnClickListener(this);
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.user_profile)
+        {
+            Intent i = new Intent(this, UserProfile.class);
+            startActivity(i);
+        }
+        else if (id == R.id.settings)
+        {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
+        }
+        else if (id == R.id.about)
+        {
+            Intent i = new Intent(this, AboutActivity.class);
+            startActivity(i);
+        }
+        else if (id == R.id.log_out)
+        {
+            Context context = getApplicationContext();
+            CharSequence text = "Successfully logged out - left authentication!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+
+        return true;
+    }
 
     @Override
     public void onClick(View view) {
@@ -58,4 +105,5 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
                 break;
         }
     }
+
 }
