c0b22cc68b32a8a9e26ee44a1e645838af216da3
michael.stokes <cse3311.team3@gmail.com>
2021-09-28 14:08:32 -0500

    Map page

--- app/src/main/java/com/example/cse3311_project/ViewMap.java
+++ app/src/main/java/com/example/cse3311_project/ViewMap.java
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ViewMap extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.cse3311_project.databinding.ActivityViewMap2Binding;
+
+public class ViewMap extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_map);
+        setContentView(R.layout.activity_view_map2);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+
+        mapFragment.getMapAsync(this);
+
+
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng UTA = new LatLng(032.73575146606, -97.1171806613291);
+        googleMap.addMarker(new MarkerOptions()
+                .position(UTA)
+                .title("UTA"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UTA, 15));
+    }
+
+
 }
