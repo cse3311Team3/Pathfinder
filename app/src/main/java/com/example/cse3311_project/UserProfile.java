089f63e51ad9765926fce177ec39c4b58855647a
michael.stokes <cse3311.team3@gmail.com>
2021-10-17 20:47:52 -0500

    User profile working as intended

--- app/src/main/java/com/example/cse3311_project/UserProfile.java
+++ app/src/main/java/com/example/cse3311_project/UserProfile.java
@@ -1,35 +1,52 @@
 package com.example.cse3311_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
+
+    Uri imageUri;
 
     EditText firstNameInput;
     EditText lastNameInput;
     EditText userNameInput;
 
+    ImageView avatarPhoto;
     Button updateAccount;
     Button cancel;
+    Button avatar;
 
     String firstName;
     String lastName;
     String userName;
 
+    private static final int IMAGE = 100;
+
     private FirebaseAuth firebaseAuth;
     private DatabaseReference firebaseRoot;
+
+    String popFirst, popLast, popUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +59,41 @@ public class UserProfile extends AppCompatActivity {
         FirebaseUser user = firebaseAuth.getCurrentUser();
         String uid = user.getUid();
 
+
+
+        firebaseRoot.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                popFirst = snapshot.child(uid).child("Account Info").child("First Name").getValue().toString();
+                popLast = snapshot.child(uid).child("Account Info").child("Last Name").getValue().toString();
+                popUser = snapshot.child(uid).child("Account Info").child("Username").getValue().toString();
+                firstNameInput.setText(popFirst);
+                lastNameInput.setText(popLast);
+                userNameInput.setText(popUser);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UserProfile.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        avatarPhoto = findViewById(R.id.avatarProfile);
         firstNameInput = findViewById(R.id.firstNameUpdate);
         lastNameInput = findViewById(R.id.lastNameUpdate);
         userNameInput = findViewById(R.id.userNameUpdate);
         updateAccount = findViewById(R.id.updateAccountButton);
+        avatar = findViewById(R.id.changeAvatar);
         cancel = findViewById(R.id.cancelAccountButton);
 
+
+
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                avatarFile();
+            }
+        });
         updateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,5 +121,17 @@ public class UserProfile extends AppCompatActivity {
             }
         });
     }
+    private void avatarFile(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == IMAGE){
+            imageUri = data.getData();
+            avatarPhoto.setImageURI(imageUri);
+        }
+    }
 }
