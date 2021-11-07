f9b3d09bb1bb31a8c3fb2ce351cd031813014b7d
Yunika Upadhayaya <yupadhayaya08@gmail.com>
2021-11-07 14:19:13 -0600

    changed hint for editTexts and removed toolbox from other pages

--- app/src/main/java/com/example/cse3311_project/CreateSchedule.java
+++ app/src/main/java/com/example/cse3311_project/CreateSchedule.java
@@ -45,8 +45,8 @@ public class CreateSchedule extends AppCompatActivity implements AdapterView.OnI
         setContentView(R.layout.activity_create_schedule);
 
         // adding toolbar to the create schedule page
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         //create_schedule window pop-up
         create_button = (Button)findViewById(R.id.create_schedule);
@@ -65,45 +65,45 @@ public class CreateSchedule extends AppCompatActivity implements AdapterView.OnI
         });
     }
     // menu inflater
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return true;
-    }
-
-    // menu options
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if(id == R.id.user_profile)
-        {
-            Intent i = new Intent(this, UserProfile.class);
-            startActivity(i); // if user clicks user profile, go to user profile page
-        }
-        else if (id == R.id.settings)
-        {
-            Intent i = new Intent(this, SettingsActivity.class);
-            startActivity(i); // if user clicks settings, go to settings page
-        }
-        else if (id == R.id.about)
-        {
-            Intent i = new Intent(this, AboutActivity.class);
-            startActivity(i); // if user clicks about, go to about page
-        }
-        else if (id == R.id.log_out)
-        {
-            Context context = getApplicationContext();
-            CharSequence text = "Successfully logged out - left authentication!"; //firebase authentication left
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-            Intent i = new Intent(this, MainActivity.class);
-            startActivity(i); //go to login page
-        }
-
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu,menu);
+//        return true;
+//    }
+//
+//    // menu options
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if(id == R.id.user_profile)
+//        {
+//            Intent i = new Intent(this, UserProfile.class);
+//            startActivity(i); // if user clicks user profile, go to user profile page
+//        }
+//        else if (id == R.id.settings)
+//        {
+//            Intent i = new Intent(this, SettingsActivity.class);
+//            startActivity(i); // if user clicks settings, go to settings page
+//        }
+//        else if (id == R.id.about)
+//        {
+//            Intent i = new Intent(this, AboutActivity.class);
+//            startActivity(i); // if user clicks about, go to about page
+//        }
+//        else if (id == R.id.log_out)
+//        {
+//            Context context = getApplicationContext();
+//            CharSequence text = "Successfully logged out - left authentication!"; //firebase authentication left
+//            int duration = Toast.LENGTH_SHORT;
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//            Intent i = new Intent(this, MainActivity.class);
+//            startActivity(i); //go to login page
+//        }
+//
+//        return true;
+//    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
