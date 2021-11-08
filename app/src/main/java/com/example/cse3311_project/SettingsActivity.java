7b00dfe845771123f552b16ea1efee7e51a3732f
Yunika Upadhayaya <yupadhayaya08@gmail.com>
2021-11-07 19:47:31 -0600

    no settings page - final for Iteration 3

--- app/src/main/java/com/example/cse3311_project/SettingsActivity.java
+++ app/src/main/java/com/example/cse3311_project/SettingsActivity.java
@@ -2,13 +2,27 @@ package com.example.cse3311_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Window;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private int brightness;
+    private ContentResolver contentResolver;
+    private Window window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
     }
 }
