ca8ac8d3892ce9cae34df6117f8d7eb3b5c3abdb
DeanKamel <odean1@hotmail.com>
2021-11-07 19:40:12 -0600

    Map (device location)

--- app/src/main/java/com/example/cse3311_project/ViewMap.java
+++ app/src/main/java/com/example/cse3311_project/ViewMap.java
@@ -1,9 +1,18 @@
 package com.example.cse3311_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -11,14 +20,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.cse3311_project.databinding.ActivityViewMap2Binding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+//import com.example.cse3311_project.databinding.ActivityViewMap2Binding;
+
 
 public class ViewMap extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_map2);
+        setContentView(R.layout.activity_maps);
 
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -26,19 +42,50 @@ public class ViewMap extends AppCompatActivity implements OnMapReadyCallback {
 
 
         mapFragment.getMapAsync(this);
-
-
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        LatLng UTA = new LatLng(032.73575146606, -97.1171806613291);
-        googleMap.addMarker(new MarkerOptions()
-                .position(UTA)
-                .title("UTA"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UTA, 15));
+        mMap = googleMap;
+        getDeviceLocation();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+//        LatLng UTA = new LatLng(-34.0, 151.0);
+//        mMap.addMarker(new MarkerOptions()
+//                .position(UTA)
+//                .title("UTA"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(UTA));
     }
 
 
-}
+    private void getDeviceLocation(){
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Location currentLocation = (Location) task.getResult();
+
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 17f));
+
+                        }else{
+                            Toast.makeText(ViewMap.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            //}
+        }catch (SecurityException e){
+        }
+    }
+
+
+    }
+
